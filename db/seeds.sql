7051657087e5627c581424278db895eb3af38ab4
Michael Gerov <mhgerov@gmail.com>
2018-04-23 08:53:01 -0400

    initiated database

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Elvis",false); 
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Bacon & Cheese",false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie",false);
