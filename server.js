c5ce01373ed11ac40dbd429750a9d62de771c5d1
Michael Gerov <mhgerov@gmail.com>
2018-04-24 12:52:42 -0400

    integrated mockup into exprhb

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
+app.use(express.static('public'));
+
 app.use('/',router);
 
 app.listen(PORT, () => console.log('Listening on port '+PORT));
